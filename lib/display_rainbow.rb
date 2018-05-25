6cf7ae497c99850fe0f307322aeab4d710b2a6f0
johnkounelias@gmail.com Kounelias <johnkounelias@gmail.com>
2018-05-25 18:35:06 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,4 @@
 def display_rainbow(colors="R: red, O: orange, Y:yellow G: green, B: blue, I: indigo, V: violet")
-enddef display_rainbow(colors="R: red, O: orange, Y:yellow G: green, B: blue, I: indigo, V: violet")
+
+end
 end
